51d9efdb7871931669a328c4a2e179b34e60dd37
王宇 <shoujifl@163.com>
2023-04-23 10:09:36 +0800

    init

---
+++ pages/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="container">
+    1
+  </view>
+</template>
+
+<script>
+  export default{
+    name:'HomeView',
+    data(){
+      return{}
+    },
+    methods:{}
+  }
+</script>
+
+<style lang="less" scoped>
+</style>
